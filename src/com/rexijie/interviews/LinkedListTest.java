20d987ba0f65d835687aa1ea2db28165087e3435
Rex Ijiekhuamen <dev.rexijie@gmail.com>
2020-07-06 08:46:09 +0100

    LinkedList Implementation. reverse Hash tag algorithm

--- src/com/rexijie/interviews/LinkedListTest.java
+++ src/com/rexijie/interviews/LinkedListTest.java
@@ -4,9 +4,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class LinkedListTest<T> {
-
-
-    private LinkedList<T> removeDuplicates (LinkedList<T> list) {
+    private LinkedList<T> removeDuplicates(LinkedList<T> list) {
         var it = list.iterator();
         T last = it.next();
         T next;
@@ -21,9 +19,25 @@ public class LinkedListTest<T> {
     }
 
     public static void main(String[] args) {
-        var list = new LinkedList<>(List.of("Rex", "Rex", "Rex", "Rupa","Rupa", "Samantha", "Samantha", "Andie" ,"Andie", "Andie"));
+        var list = new LinkedList<>(List.of("Rex", "Rex", "Rex", "Rupa", "Rupa", "Samantha", "Samantha", "Andie", "Andie", "Andie"));
         LinkedListTest<String> sL = new LinkedListTest<>();
         System.out.println(sL.removeDuplicates(list));
         int i = 0;
+    }
+}
+
+class HashTag {
+    public static void main(String[] args) {
+        int n = 500;
+
+        var space = " ";
+        var hash = "#";
+
+        for (int i = 1; i <= n; i++) {
+            var str = space.repeat(n - i)
+                    .concat(hash.repeat(i));
+            System.out.println(str);
+
+        }
     }
 }
